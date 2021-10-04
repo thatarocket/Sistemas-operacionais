c9b386a63746bf5e70287c688fdea69d6e92a66b
Melissa <melissainui@gmail.com>
2021-10-04 00:09:44 -0300

    Update BCP.java

--- BCP.java
+++ BCP.java
@@ -1,14 +1,18 @@
 public class BCP {
     private String nome;    //nome do programa
     private String estadoProcesso;  //estado que o programa se encontra (executando, bloqueado ou pronto)
-    private String registradorX;    //estado do registrador de uso GERAL X
-    private String registradorY;    //estado do registrador de uso GERAL Y
+    private int registradorX;    //estado do registrador de uso GERAL X
+    private int registradorY;    //estado do registrador de uso GERAL Y
     private int contadorPrograma;    //contador de programa (registrador de uso ESPECIFICO) - posicao do arranjo segTextProg
-    private String [] segTextoProg;    //segmento de texto do programa;
+    private String[] segTextoProg;    //segmento de texto do programa;
+    private int tempoEspera;
 
-    public BCP(String nome, String[]segTextoProg){
+    public BCP(String nome, String[] segTextoProg) {
         this.nome = nome;
         this.segTextoProg = segTextoProg;
+        this.tempoEspera = 0;
+        this.estadoProcesso = "executando";
+        this.contadorPrograma = 0;
     }
 
 
@@ -18,15 +22,19 @@ public class BCP {
         return nome;
     }
 
+    public int getTempoEspera() {
+        return tempoEspera;
+    }
+
     public String getEstadoProcesso() {
         return estadoProcesso;
     }
 
-    public String getRegistradorX() {
+    public int getRegistradorX() {
         return registradorX;
     }
 
-    public String getRegistradorY() {
+    public int getRegistradorY() {
         return registradorY;
     }
 
@@ -45,11 +53,11 @@ public class BCP {
         this.estadoProcesso = estadoProcesso;
     }
 
-    public void setRegistradorX(String registradorX) {
+    public void setRegistradorX(int registradorX) {
         this.registradorX = registradorX;
     }
 
-    public void setRegistradorY(String registradorY) {
+    public void setRegistradorY(int registradorY) {
         this.registradorY = registradorY;
     }
 
@@ -57,4 +65,7 @@ public class BCP {
         this.contadorPrograma = contadorPrograma;
     }
 
+    public void setTempoEspera(int tempoEspera) {
+        this.tempoEspera = tempoEspera;
+    }
 }
