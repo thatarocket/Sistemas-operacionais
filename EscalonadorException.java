8e044524b2d90266b9200c4e829b3e1ec14027f4
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-10-17 15:37:21 -0300

    Update EscalonadorException.java
    Tava escrito participante hahaha do outro ep

--- EscalonadorException.java
+++ EscalonadorException.java
@@ -2,13 +2,13 @@ public class EscalonadorException extends Throwable {
     private static final long serialVersionUID = 22222222222222L;
     private String msg;
 
-    // constrói um objeto ParticipanteException com a mensagem passada por parâmetro
+    // constrói um objeto EscalonadorException com a mensagem passada por parâmetro
     public EscalonadorException(String msg) {
         super(msg);
         this.msg = msg;
     }
 
-    // contrói um objeto ParticanteException com mensagem e a causa dessa exceção, utilizado para encadear exceptions
+    // contrói um objeto EscalonadorException com mensagem e a causa dessa exceção, utilizado para encadear exceptions
     public String getMessage() {
         return this.msg;
     }
