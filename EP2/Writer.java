5bff1e303833a7acffe2a5456e6739da312f229a
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-15 18:50:18 -0300

    Update Writer.java

--- EP2/Writer.java
+++ EP2/Writer.java
@@ -1,6 +1,16 @@
-public class Writer extends Thread {
+import java.util.ArrayList;
+
+public class Writer extends Object {
+//Escrevem na base
 	
+	public Writer(ArrayList<String> words,int posicBase) {
+		words.set(posicBase,"MODIFICADO");
+	}
+
 	@Override
-	public void run() {}
+	public void run() {
+		
+
+	}
 	
 }
