50999796278aaab8ab5053d55073caa0b472d2f1
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-16 00:02:24 -0300

    Update Reader.java

--- EP2/Reader.java
+++ EP2/Reader.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 
-public class Reader extends Object {
+public class Reader extends ObjectThread {
 //Acessa a base para a leitura
 
 	private static String currentWord;
 
-	public Reader(ArrayList<String> words,int posicBase) {
+	public void doAction(ArrayList<String> words,int posicBase) {
 		this.currentWord = words.get(posicBase);
+		//System.out.println("READER");
 	}
 
 	@Override
 	public void run() {
-		
+
 	}
 
 }
