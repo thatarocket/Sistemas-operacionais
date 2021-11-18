a0a6260601f26042dcf894d5fd71822be2bdbf64
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-18 01:58:10 -0300

    Update Writer.java

--- EP2/Writer.java
+++ EP2/Writer.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Writer extends ObjectThread {
 //Escrevem na base
 
-	public void doAction(ArrayList<String> words,int posicBase) {
+	public void acessFiles(ArrayList<String> words,int posicBase) {
 		words.set(posicBase,"MODIFICADO");
-		//System.out.println("WRITER");
+	}
+
+	public void lock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {
+		writeSemaphore.acquire();
+	}
+	public void unlock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {
+		writeSemaphore.release();
 	}
 
 	@Override
