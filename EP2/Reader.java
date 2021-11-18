d577db27b4dbf7ab8af3371913387c4bc8563199
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-18 01:57:18 -0300

    Update Reader.java

--- EP2/Reader.java
+++ EP2/Reader.java
@@ -1,17 +1,36 @@
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 
 public class Reader extends ObjectThread {
 //Acessa a base para a leitura
 
 	private static String currentWord;
+	protected static ReadWriteLock dataBase;
 
-	public void doAction(ArrayList<String> words,int posicBase) {
+	public void acessFiles(ArrayList<String> words,int posicBase) {
 		this.currentWord = words.get(posicBase);
-		//System.out.println("READER");
+	}
+
+	public void lock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException  {
+		readSemaphore.acquire();
+		if(threadsReads == 0){
+			writeSemaphore.acquire();
+		}
+		threadsReads++;
+		readSemaphore.release();
+	}
+	public void unlock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {
+		readSemaphore.acquire();
+        threadsReads--;
+        if(threadsReads == 0){
+          writeSemaphore.release();
+        }
+        readSemaphore.release();
 	}
 
 	@Override
 	public void run() {
+		
 
 	}
 
