98f164b4f34fd5bd4b1ef737a64aa3e0d6518116
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-18 01:56:45 -0300

    Update ObjectThread.java

--- EP2/ObjectThread.java
+++ EP2/ObjectThread.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
 public class ObjectThread extends Thread {
 
-	public void doAction(ArrayList<String> words,int posicBase){}
+	public void acessFiles(ArrayList<String> words,int posicBase){}
+	public void lock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {}
+	public void unlock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {}
+	
 	@Override
 	public void run(){}
 }
