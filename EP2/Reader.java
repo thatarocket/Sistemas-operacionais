d0b767cb2ebf8f7bbab098ba3ef7b6719f2501fc
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-19 16:44:18 -0300

    Update Reader.java

--- EP2/Reader.java
+++ EP2/Reader.java
@@ -1,36 +1,72 @@
+/**
+ * EP 2 - Sistemas Operacionais (Turma 94)
+ *
+ * @author Thais de Souza Rodrigues, 11796941
+ * @author Melissa Akie Inui, 11908865
+ * @author Silas Bovolin Reis, 11796739
+ * @author Gabriel de Oliveira Theodoro, 11202569
+ */
+
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
+/**
+ * Acessa a base para a leitura
+ */
 public class Reader extends ObjectThread {
-//Acessa a base para a leitura
 
-	private static String currentWord;
-	protected static ReadWriteLock dataBase;
+  protected static String currentWord;
 
+  /**
+   * Acessa os arquivos, lê a posicao da base e a armazena
+   * @param ArrayList<String> words
+   * @param int posicBase
+   * @return void
+   */
 	public void acessFiles(ArrayList<String> words,int posicBase) {
 		this.currentWord = words.get(posicBase);
 	}
 
-	public void lock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException  {
-		readSemaphore.acquire();
-		if(threadsReads == 0){
-			writeSemaphore.acquire();
-		}
-		threadsReads++;
-		readSemaphore.release();
-	}
-	public void unlock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {
-		readSemaphore.acquire();
-        threadsReads--;
-        if(threadsReads == 0){
-          writeSemaphore.release();
-        }
-        readSemaphore.release();
-	}
+  /**
+   * Trava o acesso a base do escritor, para o leitor acessar
+   * @param int threadsReads
+   * @param ArrayList<String> words
+   * @param int posicBase
+   * @param Semaphore readSemaphore
+   * @param Semaphore writeSemaphore
+   * @throws InterruptedException
+   * @return void
+   */
+  public synchronized void lock(int threadsReads,ArrayList<String> words,int posicBase,Semaphore readSemaphore,Semaphore writeSemaphore) throws InterruptedException {
+    readSemaphore.acquire();
+    if(threadsReads == 0){
+      writeSemaphore.acquire();
+    }
+    threadsReads++;
+    acessFiles(words,posicBase);
+    readSemaphore.release();
+  }
+
+  /**
+   * Desbloqueia o acesso a base
+   * @param int threadsReads
+   * @param Semaphore readSemaphore
+   * @param Semaphore writeSemaphore
+   * @throws InterruptedException
+   * @return void
+   */
+  public synchronized void unlock(int threadsReads,Semaphore readSemaphore,Semaphore writeSemaphore) throws InterruptedException {
+    readSemaphore.acquire();
+    threadsReads--;
+    if(threadsReads == 0){
+      writeSemaphore.release();
+    }
+    readSemaphore.release();
+  }
 
 	@Override
 	public void run() {
-		
+	
 
 	}
 
