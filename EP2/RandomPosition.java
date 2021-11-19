0b22e173b434a0423aa8b5dd7692a2128d5bde9b
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-19 16:42:07 -0300

    Update RandomPosition.java

--- EP2/RandomPosition.java
+++ EP2/RandomPosition.java
@@ -1,6 +1,15 @@
+/**
+ * EP 2 - Sistemas Operacionais (Turma 94)
+ *
+ * @author Thais de Souza Rodrigues, 11796941
+ * @author Melissa Akie Inui, 11908865
+ * @author Silas Bovolin Reis, 11796739
+ * @author Gabriel de Oliveira Theodoro, 11202569
+ */
+
 import java.util.concurrent.ThreadLocalRandom;
 
-public class RandomPosition { 
+public class RandomPosition {
 
 	public int getRandom(int size) {
 		ThreadLocalRandom random = ThreadLocalRandom.current();
