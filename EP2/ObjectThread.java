d67549f148f31bcf962dc5c00621ca7db82b8422
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-19 16:41:50 -0300

    Update ObjectThread.java

--- EP2/ObjectThread.java
+++ EP2/ObjectThread.java
@@ -1,12 +1,21 @@
+/**
+ * EP 2 - Sistemas Operacionais (Turma 94)
+ *
+ * @author Thais de Souza Rodrigues, 11796941
+ * @author Melissa Akie Inui, 11908865
+ * @author Silas Bovolin Reis, 11796739
+ * @author Gabriel de Oliveira Theodoro, 11202569
+ */
+
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 
 public class ObjectThread extends Thread {
 
 	public void acessFiles(ArrayList<String> words,int posicBase){}
-	public void lock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {}
-	public void unlock(int threadsReads,Semaphore readSemaphore, Semaphore writeSemaphore) throws InterruptedException {}
-	
+	public synchronized void lock(int threadsReads,ArrayList<String> words,int posicBase,Semaphore readSemaphore,Semaphore writeSemaphore) throws InterruptedException {}
+	public synchronized void unlock(int threadsReads,Semaphore readSemaphore,Semaphore writeSemaphore) throws InterruptedException {}
+
 	@Override
 	public void run(){}
 }
