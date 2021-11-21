4194b5b9fa3d9904210f05349166154aec67c191
Thaís de Souza <71095424+thatarocket@users.noreply.github.com>
2021-11-21 01:48:48 -0300

    Update RandomPosition.java

--- EP2/RandomPosition.java
+++ EP2/RandomPosition.java
@@ -11,6 +11,11 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomPosition {
 
+   /**
+    * Gera uma posicao aleatoria, de acordo com o tamanho enviado
+    * @param int size
+    * @return int
+    */
 	public int getRandom(int size) {
 		ThreadLocalRandom random = ThreadLocalRandom.current();
 		return random.nextInt(0,size);
